ae1d744d0afa21421f36fd3b3fd2bfbc5f7ab15b
ameliasg <160703235+ameliasg@users.noreply.github.com>
2024-09-08 13:16:19 +0700

    get all jobs

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
+import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
-import * as dotenv from 'dotenv';
+import { nanoid } from 'nanoid';
 
-const getData = async () => {
-
-}
-getData()
-
-try {
-    const response = await fetch('https://www.course-api.com/react-useReducer-cart-project');
-    const cartData = await response.json()
-    console.log(cartData)
-} catch (error) {
-    console.log(error);
-    
-}
+let jobs = [
+    { id: nanoid(), company: 'apple', position: 'front-end' },
+    { id: nanoid(), company: 'google', position: 'back-end' },
+]
 
 if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
@@ -32,6 +24,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     console.log(req);
     res.json({ message: 'data received', data: req.body });
+})
+
+app.get('/api/v1/jobs', (req, res) => {
+    res.status(200).json({jobs})
 })
 
 const port = process.env.PORT || 5100
