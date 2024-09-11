0ea6e125208d0a8700242087738262635d5e7329
ameliasg <160703235+ameliasg@users.noreply.github.com>
2024-09-12 00:07:08 +0700

    error handler

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import 'express-async-errors'  
+import 'express-async-errors'
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
@@ -8,6 +8,9 @@ import mongoose from 'mongoose'
 
 // Routers
 import jobRouter from './routes/jobRouter.js'
+
+// middleware
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -31,10 +34,7 @@ app.use('*', (req, res) => {
     res.status(404).json({ msg: 'not found' })
 })
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({ msg: 'something went wrong' })
-})
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5100
 
